bbdfabd2165200846184210a49bc5ad2f2cbed4e
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2023-12-19 21:52:00 +0530

    Project 7 (part 1 + 2)

---
+++ TAILWIND/Project 7(paytmKaro)/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        colors :{
+            nilla: "#00baf2",
+            darkNilla: '#002970',
+            lightColor:'#f5f5f5',
+            lightNilla:'#00afe3',
+            lightDarkNilla:'#0f4a8a',
+            bgColor:'#f5f7fa'
+        }
+    },
+  },
+  plugins: [],
+}
+
