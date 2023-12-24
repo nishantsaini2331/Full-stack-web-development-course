7468b0237b5143d194916ebca92a29cde4ed0d45
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2023-12-24 13:16:49 +0530

    complete Paytm Project

---
+++ TAILWIND/Project 7(paytmKaro)/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+        colors :{
+            nilla: "#00baf2",
+            darkNilla: '#002970',
+            lightColor:'#f5f5f5',
+            lightNilla:'#00afe3',
+            lightDarkNilla:'#0f4a8a',
+            bgColor:'#f5f7fa',
+            ultalightNilla :'#4ccff6',
+            line:'#cccccc',
+            fontColor:'#505050',
+            smallFontColor:'#707086'
+        }
+    },
+  },
+  plugins: [],
+}
+
