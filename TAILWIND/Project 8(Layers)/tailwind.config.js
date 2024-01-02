20e65a8bc3be2a3150b65dfebb39c4186b9317da
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-02 23:20:29 +0530

    Complete Layers project

---
+++ TAILWIND/Project 8(Layers)/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["*"],
+    theme: {
+      extend: {
+        screens :{
+            'extraSmall':{'max':'420px'},
+            'vsmall':{'max':'750px'},
+            'footer':{'max':'950px'},
+            'small':{'max':"1200px"}
+        },
+        scrollbars : {
+
+        }
+      },
+    },
+    plugins: [],
+  }
