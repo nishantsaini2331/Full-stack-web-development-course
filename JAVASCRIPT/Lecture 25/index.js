9120a98444c22263aa0014ce8aa58b139a635b6a
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-06 18:50:44 +0530

    Javascript started

---
+++ JAVASCRIPT/Lecture 25/index.js
@@ -0,0 +1,41 @@
+
+// a = "hello";
+// nishant = 'code thread'
+// console.log(nishant, a);
+
+// re-declare and updation is possible , global scope
+// var a = 10;
+// var a = 20;
+// console.log(a);
+
+
+// let a; // declaralation 
+//  a = 5; // initialization
+//  let b = 6 ; // dono kaam ek sath
+
+// updation is possible but not redeclaration , local scope
+// let a = 20;
+//     a = 22;
+// console.log(a);
+
+// updation and redeclaration is not possible , local scope
+// const adharID = 8346638635865;
+//  adharID = 756578346047;
+// console.log(adharID);
+
+
+
+// let 1a = 7;
+// let $ad = 9;
+// let a = 4
+// let A = 4;
+
+// let akash ambani  = 'rupya'
+let akashAmbani$ = 'rupya'
+let akash_ambani = 'rupya he rupya'
+console.log(akash_ambani);
+
+
+
+
+
