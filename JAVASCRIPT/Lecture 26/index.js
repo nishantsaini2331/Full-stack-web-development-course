517394162702b538b03b1fed8599031471e21f82
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-09 13:30:16 +0530

    lecture 26

---
+++ JAVASCRIPT/Lecture 26/index.js
@@ -0,0 +1,85 @@
+
+
+// let num = 20 ;
+// console.log(typeof(num))
+
+// let str = 'codeThread'
+// console.log(typeof str);
+
+
+// let bool = false
+// console.log(typeof bool)
+
+// let a = null;
+// console.log( a)
+
+// let b;
+// console.log(b)
+
+// let a = Symbol("codeThread")
+// console.log(a)
+
+// let big = BigInt(123)
+// console.log(big)
+
+
+// const ytLecture = {
+//     video : 'https://hbhlgelevhl',
+//     thumbnail:"https://ngkfddsc",
+//     descriptionn:"iuw3tyvnwrvgtcncathtvamhnlv",
+//     tags: "lec26, js course, basic to advance",
+// }
+// console.log( ytLecture)
+
+
+// arithmatic operator 
+
+// let sum = 4 + 7 ;
+// console.log(sum)
+
+// const num1 = 5 ;
+// const num2  = 20 ;
+
+// let multi = num1 * num2 ;
+// console.log(multi)
+
+
+// let div  = 3 % 2 ;
+// console.log(div)
+
+// let pow = 2 ** 5;
+// console.log(pow)
+
+
+// let channel = 'codeThread'
+
+// let val = 45 ;
+
+
+// let val = 4;
+//  val /= 2 ; // val = val / 2 
+// console.log(val)
+
+
+// let count = 4 ;
+// console.log("first count",--count)
+// console.log("second count",count)
+
+// console.log(2 && 0)
+// console.log(0 && -2)
+// console.log(2 || 0)
+// console.log(0 || 2)
+
+
+
+// let com = 4 !== '4' ;
+// console.log(com)
+
+// let sum = '1' + 1 + 1  ;
+// console.log( sum);
+
+// let b;
+// console.log( typeof(typeof(b)))
+
+
+
