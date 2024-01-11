b79901a09ac5369556adcdf23e8d2b158b2d9cba
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-11 14:23:28 +0530

    Lecture 27

---
+++ JAVASCRIPT/Lecture 27/index.js
@@ -0,0 +1,83 @@
+// let age = 14;
+
+// if (age <= 10 ) {
+//   console.log("chota bacha ");
+// } else if (age >= 11 && age <= 17) {
+//   console.log("thoda bada bacha ");
+// } else if (age >= 18 && age <= 30) {
+//   console.log("Paisa kaamo");
+// } else {
+//   console.log("Maje lo ");
+// }
+
+// let expression = '-'
+// const num1 = 10;
+// const num2 = 20;
+
+// switch(expression){
+//     case '+' :  console.log(num1 + num2);
+//                 break;
+
+//     case '-' :  console.log(num1 - num2);
+//                 break;
+//     default : console.log("choose correct operator ");
+//                 break;
+
+// }
+
+// let num1 = 4 ;
+// let num2 = '4';
+// if(num1 == num2  && typeof(num1) == typeof(num2) ) {
+//     console.log("true")
+// }else{
+//     console.log("false")
+// }
+
+// for(let i = 1 ; i<= 10  ; i++){
+//     console.log(i * 15);
+// }
+
+// let i = 11;
+
+// while(i<= 10){
+//     console.log(i * 15);
+//     i++;
+// }
+
+// let i = 11;
+
+// do{
+//     console.log(i * 1);
+//     i++;
+// }while(i<= 10)
+
+// ternary operator
+
+// console.log(4 < 9 ? "true" : "false");
+
+// for (let i = 0; i <= 10; i++) {
+    
+//     if(i == 4){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+
+//     if(i == 4){
+//         continue;
+//     }
+//     console.log(i)
+    
+// }
+
+
+for(let i = 0 ; i<= 5 ; i++){
+    str = ''
+    for(let j = 0 ; j <= 5 ; j++){
+        str += '*'
+    }
+    console.log(str)
+}
---
+++ JAVASCRIPT/Lecture 27/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+for(let i = 0 ; i <= 5 ; i++){
+    console.log(i);
+}
