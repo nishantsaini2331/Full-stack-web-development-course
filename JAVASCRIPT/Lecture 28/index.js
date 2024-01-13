f16e47e74d2558a68c649da1d2eff44d59ec17b5
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-13 21:10:47 +0530

    Lecure 28

---
+++ JAVASCRIPT/Lecture 28/index.js
@@ -0,0 +1,60 @@
+// function totalSum(english, hindi, maths) {
+//   let sum = english + hindi + maths;
+//   return sum;
+
+// }
+
+// let amanTotal = totalSum(50, 70, 100);
+
+
+// let num = 200.5634;
+// console.log(num.toFixed(2))
+// console.log(num.toPrecision(5)
+
+
+// let number = new Number(100)
+// console.log(number.toPrecision(4))
+
+
+// let str = "Heelo"
+// console.log(str)
+
+// let str = new String("Heelo")
+
+
+let str = "Code Thread"
+// console.log(str.replace(" ","EEE"))
+// console.log(str.split("e"))
+// console.log(str.length)
+// console.log(str.toLowerCase())
+// console.log(str.toUpperCase())
+// console.log(str.match("Code"))
+// console.log(str.charAt(5))
+// console.log(str.indexOf("Thread"))
+
+// console.log(Math)
+
+// console.log(Math.floor(3.99999))
+// console.log(Math.ceil(3.99999))
+// console.log(Math.max(3,35,6))
+// console.log(Math.min(3,35,6))
+// console.log(Math.round(6.5))
+// console.log(Math.abs(-5))
+// console.log(Math.random())
+
+
+// let randomNumber = Math.floor(Math.random() * 10 ) + 1; 
+
+// let min = 10 ;
+// let max = 20;
+// let randomNumber = Math.floor(Math.random() * (max- min + 1) )+ min;
+// console.log(randomNumber);
+
+
+let date = new Date();
+
+// console.log(Math.round(date.getTime()/1000))
+// console.log(date.toISOString())
+// console.log(date.toLocaleString())
+
+
