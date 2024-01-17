8c695db5e055bbe8bc7ac4ed2c668301c8b3cf39
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-17 14:35:20 +0530

    Lecture 29

---
+++ JAVASCRIPT/Lecture 29/index.js
@@ -0,0 +1,186 @@
+// let arr = [2, 4, 56, 24, 57, 25];
+// console.log(typeof arr);
+
+// let language = ["Cpp", "JAVA", "Rust", "C", "JAVASCRIPT"];
+
+// console.log(language[0]);
+
+// let random  = [1 , "Hello" , "Sir" , true , {name:"Nishant"} , 22]
+
+// console.log(typeof random[4])
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// arr.push(40,90,50)
+// arr.pop()
+//  let newArr = arr.concat(45,67,12)
+// console.log(newArr)
+//  let ret =  arr.indexOf(8)
+
+//  let newArr  =  arr.join("-")
+
+// console.log(arr.length)
+
+// console.log(arr.reverse())
+// arr.shift()
+// arr.unshift(3,4,5)
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let newArr = arr.slice(1, 4)
+// console.log(arr)
+// console.log(newArr)
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let newArr = arr.splice(1, 4)
+// console.log(arr)
+// console.log(newArr)
+
+// let arr = [45,64,3,34,5,3]
+
+// for(let i = 0 ; i < arr.length; i++ ){
+//     console.log(arr[i])
+// }
+
+let arr4 = [45,64,3,34,5,3]
+let [first , second , third , fourth, ...remaining] = arr4;
+console.log(remaining)
+
+// const obj = {
+//   name: "Nishant",
+//   id: 23134563,
+//   course: "B.tech",
+//   Branch: "Cse",
+//   hosteller: false,
+// };
+// console.log(obj["name"])
+// console.log(obj.id)
+
+// const obj2 = {
+//   name: "Nishant2",
+//   id: 23134563,
+//   course: "B.tech",
+//   Branch: "Cse",
+//   hosteller: false,
+// };
+
+// const student = [
+//   {
+//     name: "Nishant",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant2",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant2",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant2",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+//   {
+//     name: "Nishant2",
+//     id: 23134563,
+//     course: "B.tech",
+//     Branch: "Cse",
+//     hosteller: false,
+//   },
+// ];
+
+const obj = {
+    name: "Nishant",
+    id: 23134563,
+    course: "B.tech",
+    Branch: "Cse",
+    hosteller: false,
+  };
+
+ const obj2 = {
+    name: "Nishant",
+    id: 23134563,
+    course: "B.tech",
+    Branch: "Cse",
+    hosteller: false,
+  };
+
+//   const {name, id, course , ...hello} = obj2;
+//   console.log(hello)
+
+let arr = [45,64,3,34,5,3]
+
+let arr2 = [24,23566,2]
+
+// const newArr = arr.concat(arr2)
+// console.log(newArr)
+
+// let newArr = [...arr , ...arr2]
+// console.log(newArr)
+
+// for(let val of arr){
+//     console.log(val)
+// }
+
+// for(const val of Object.keys(obj)){
+//     console.log(val)
+// }
+
+// for(const val of Object.values(obj)){
+//     console.log(val)
+// }
+
+// for(let val in arr){
+//     console.log(arr[val])
+// }
+
+// for(let val in obj){
+//     console.log(val)
+// }
+
+// let str = 342263
+// console.log(`${str} ji`)
+// let str = "Heloo
+// ieruvggvghm8g
+// "
+
+// let str = `hello`
+
+// console.log(`${str}`)
+
+
+
+
+
