3d4b7e87dd51e2d0a098c478dce458d1add5edcb
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-21 13:29:36 +0530

    lec 31

---
+++ JAVASCRIPT/Lecture 31/index.js
@@ -0,0 +1,30 @@
+
+
+// let naam = "Nishant"
+
+// for (let i = 0; i < 5; i++) {
+    
+//     console.log(naam)
+// }
+
+
+function heelo1(){
+    const channelName = "CodeThread"
+    console.log("inside function")
+    
+    function helllo2 () {
+        const naam  = "Nishant"
+        console.log(channelName)
+
+        {
+            console.log(naam)
+        }
+    }
+
+    
+    helllo2()
+    
+}
+heelo1()
+
+
