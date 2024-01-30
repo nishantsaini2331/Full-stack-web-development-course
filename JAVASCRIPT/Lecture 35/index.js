88ef0a1575bcfdd180ceeaeca284e121954a786c
Nishant Saini <121441158+nishantsaini2331@users.noreply.github.com>
2024-01-30 19:09:44 +0530

    lec 35

---
+++ JAVASCRIPT/Lecture 35/index.js
@@ -0,0 +1,46 @@
+// console.log("kaise ho dost ")
+
+// let a = 2 ;
+// let b = Number("4");
+// let c = a + b;
+// console.log(c)
+
+// console.log( "45" + 9  + 3   )
+
+// console.log(7  + null)
+// console.log("7" + null)
+// console.log(4 + true)
+// console.log(5 - "56")
+// console.log("heelo" - 4)
+// console.log({} - 5 )
+// console.log([] - 6)
+
+// console.log(5 - undefined)
+
+// let obj = {
+//     a: 45,
+//     toString : function () {
+//         return "Helloji 3";
+//     },
+//     valueOf: function () {
+//         return 45;
+//     }
+// };
+// let obj = {a: 45, toString() {return "Helloji"}, valueOf(){return 50}, valueOf(){return 10}};
+
+// console.log(obj + " Dosto");
+// obj -> valueOf() - >return object ->toString() -> return
+//  [object Object] and this is string
+
+// console.log(5 - obj)
+
+
+// let obj2 = {"b":5 , valueOf() {return 60} , toString() {return "hello"}}
+
+// console.log(5 + `${obj2}`) // hind - "string"
+// console.log(typeof `${obj2}`)
+
+// let obj2 = {"b":5 , valueOf() {return {}} , toString() {return {}}}
+//error case
+
+// console.log(2 + obj2)
